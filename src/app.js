0027f7c14ac13a49962ee14ee1b97eb2169fa3d3
Tonnam Balankura <tonnam.balankura@gmail.com>
2016-10-16 23:53:13 +0000

    Working d3 v4 scatterplot

--- src/app.js
+++ src/app.js
@@ -1,81 +1,90 @@
 import * as d3 from "d3";
-// import $ from 'jquery';
 
-console.log('Loading graph');
+console.log('Loading app.js');
 
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
     width = 960 - margin.left - margin.right,
     height = 500 - margin.top - margin.bottom;
 
-var x = d3.scale.linear()
+// Scale, range [=] pixels
+var x = d3.scaleLinear()
     .range([0, width]);
 
-var y = d3.scale.linear()
+var y = d3.scaleLinear()
     .range([height, 0]);
 
-var color = d3.scale.category10();
+var color = d3.scaleOrdinal(d3.schemeCategory10);
 
-var xAxis = d3.svg.axis()
-    .scale(x)
-    .orient("bottom");
+// Axis objects
+var xAxis = d3.axisBottom(x);
 
-var yAxis = d3.svg.axis()
-    .scale(y)
-    .orient("left");
+var yAxis = d3.axisLeft(y);
 
-var svg = d3.select("body").append("svg")
+// SVG object
+var svg = d3.select("body")
+    .append("svg")
     .attr("width", width + margin.left + margin.right)
     .attr("height", height + margin.top + margin.bottom)
-  .append("g")
+    .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
+// Load data
 d3.tsv("data.tsv", function(error, data) {
   if (error) throw error;
 
+  // Convert string into number
   data.forEach(function(d) {
     d.sepalLength = +d.sepalLength;
     d.sepalWidth = +d.sepalWidth;
   });
 
+  // Domain [=] [min, max], .nice() extends domain so that it will round up
   x.domain(d3.extent(data, function(d) { return d.sepalWidth; })).nice();
   y.domain(d3.extent(data, function(d) { return d.sepalLength; })).nice();
 
+  // Create x axis line, ticks
   svg.append("g")
       .attr("class", "x axis")
       .attr("transform", "translate(0," + height + ")")
       .call(xAxis)
-    .append("text")
+  
+  // Need to separate axis label from axis line for d3 v4
+  svg.append("text")
       .attr("class", "label")
-      .attr("x", width)
-      .attr("y", -6)
+      .attr("transform", "translate(" + width + " ," + (height-10) + ")")
       .style("text-anchor", "end")
       .text("Sepal Width (cm)");
 
+  // Create y axis line, ticks
   svg.append("g")
       .attr("class", "y axis")
       .call(yAxis)
-    .append("text")
+      
+  // Create y axis label
+  svg.append("text")
       .attr("class", "label")
       .attr("transform", "rotate(-90)")
       .attr("y", 6)
       .attr("dy", ".71em")
       .style("text-anchor", "end")
-      .text("Sepal Length (cm)")
-
+      .text("Sepal Length (cm)");
+  
+  // Plot data as circles
   svg.selectAll(".dot")
       .data(data)
-    .enter().append("circle")
-      .attr("class", "dot")
-      .attr("r", 3.5)
-      .attr("cx", function(d) { return x(d.sepalWidth); })
-      .attr("cy", function(d) { return y(d.sepalLength); })
-      .style("fill", function(d) { return color(d.species); });
+      .enter().append("circle")
+        .attr("class", "dot")
+        .attr("r", 3.5)
+        .attr("cx", function(d) { return x(d.sepalWidth); })
+        .attr("cy", function(d) { return y(d.sepalLength); })
+        .style("fill", function(d) { return color(d.species); });
 
+  // Add legend
   var legend = svg.selectAll(".legend")
       .data(color.domain())
-    .enter().append("g")
-      .attr("class", "legend")
-      .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
+      .enter().append("g")
+        .attr("class", "legend")
+        .attr("transform", function(d, i) { return "translate(0," + i * 20 + ")"; });
 
   legend.append("rect")
       .attr("x", width - 18)
@@ -91,3 +100,4 @@ d3.tsv("data.tsv", function(error, data) {
       .text(function(d) { return d; });
 
 });
+
